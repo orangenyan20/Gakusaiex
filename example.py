03582a57a7ad3db32bc325c56d301aa99bf39864
orange <107176034+orangenyan20@users.noreply.github.com>
2025-06-20 11:56:17 +0900

    Add files via upload

---
+++ example.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
+client = gspread.authorize(creds)
+
+spreadsheet = client.open_by_url(st.secrets["config"]["spreadsheet_url"])
+worksheet = spreadsheet.sheet1
+worksheet.append_row(["テスト", "連携OK"])
